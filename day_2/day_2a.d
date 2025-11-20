36f77492cbeb3ebcb8f449bd800d6f8873010d4c
Jim Lawless <lawless.jim@gmail.com>
2025-11-19 19:29:47 -0600

    initial upload

---
+++ day_2/day_2a.d
@@ -0,0 +1,59 @@
+import std.stdio;
+import std.conv;
+import std.file;
+import std.array;
+import std.math.algebraic;
+import std.datetime.stopwatch;
+
+void doit(string fname) {
+    auto file = File(fname,"r");
+    int count=0;
+    foreach(line; file.byLineCopy()) {
+        auto strLevels = line.split();
+        auto levels = new int[strLevels.length];
+        for(int i=0;i<strLevels.length;i++) {
+            levels[i]=to!int(strLevels[i]);
+        }
+        int asc = ((levels[0]-levels[1])<0)? 1 : -1 ;
+        bool safe=true;
+        for(int i=1;i<levels.length;i++) {
+            int asc2 = ((levels[i-1]-levels[i])<0)? 1 : -1 ;
+            if(asc != asc2) {
+                safe=false;
+                break;
+            }
+            int diff;
+            if(asc==-1) 
+                diff=levels[i-1]-levels[i];
+            else
+                diff=levels[i]-levels[i-1];
+            if((diff<1)||(diff>3)) {
+                safe=false;
+                break;
+            }        
+        }
+        if(safe) {
+            count++;
+        }
+    }
+    writeln(count);
+    file.close();
+}
+
+
+void main(string[] args) {
+    if(args.length < 2) {
+        writeln("Syntax:\n\t",args[0]," filename\n");
+        return;
+    }
+    auto sw = StopWatch(AutoStart.no);
+    sw.start();
+    try {
+        doit(args[1]);
+    }
+    catch(FileException e) {
+        writeln("Exception: ", e.msg);
+    }
+    sw.stop();
+    writeln("Elapsed time: ",sw.peek());
+}
